dc4b3fb04ba91aba2e573c602e2eb1fa9a64c9cd
Med-lemineHmd <leminlemin00@gmail.com>
2022-10-08 20:17:39 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
-import ReactTooltip from "react-tooltip";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-
 import {
   Ecommerce,
   Orders,
@@ -14,85 +13,94 @@ import {
   Pyramid,
   Customers,
   Kanban,
+  Line,
   Area,
   Bar,
   Pie,
-  Line,
   Financial,
   ColorPicker,
   ColorMapping,
   Editor,
 } from "./pages";
+import "./App.css";
 
 import { useStateContext } from "./contexts/ContextProvider";
 
-import "./App.css";
-
 const App = () => {
-  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
   return (
-    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          {/* Bottom Settings  */}
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <button
-              data-tip="Settings"
-              type="button"
-              className="text-3xl p-3 text-white hover:drop-shadow-xl hover:bg-light-gray"
-              style={{ background: currentColor, borderRadius: "50%" }}
-              onClick={() => setThemeSettings(true)}
-            >
-              <FiSettings />
-            </button>
-            <ReactTooltip place="top" type="dark" effect="solid" />
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
           </div>
-          {/* /Bottom Settings  */}
-
-          {/* Sidebar */}
           {activeMenu ? (
-            <div className="w-72 fixed sidebar bg-white dark:bg-secondary-dark-bg">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
-            <div className="w-0 dark-secondary-dark-bg">
+            <div className="w-0 dark:bg-secondary-dark-bg">
               <Sidebar />
             </div>
           )}
-          {/* /*Sidebar */}
-
-          {/* Navbar */}
           <div
-            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72" : "flex-2"
-            }`}
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
           >
-            <div className="md-static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
-
-            {/* /Navbar */}
-
-            {/* Routing */}
             <div>
               {themeSettings && <ThemeSettings />}
+
               <Routes>
-                {/* Dashboard */}
+                {/* dashboard  */}
                 <Route path="/" element={<Ecommerce />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
 
-                {/* Pages */}
+                {/* pages  */}
                 <Route path="/orders" element={<Orders />} />
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/customers" element={<Customers />} />
 
-                {/* Apps */}
+                {/* apps  */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
-                {/* Charts */}
+                {/* charts  */}
                 <Route path="/line" element={<Line />} />
                 <Route path="/area" element={<Area />} />
                 <Route path="/bar" element={<Bar />} />
@@ -103,7 +111,7 @@ const App = () => {
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
-            {/* /Routing */}
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -5,62 +5,36 @@ import {
   SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Category"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      xName="x"
-      yName="y"
-      type={type}
-      dataSource={data}
-      // To enable tooltip for sparkline
-      tooltipSettings={{
-        visible: true,
-        // formatting tooltip text
-        format: "${x} : data ${y}",
-        trackLineSettings: {
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
           visible: true,
-        },
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-    // <SparklineComponent
-    //   id={id}
-    //   height={height}
-    //   width={width}
-    //   axisSettings={{
-    //     minX: -1,
-    //     maxX: 7,
-    //     maxY: 8,
-    //     minY: -1,
-    //   }}
-    //   fill={color}
-    //   valueType="Category"
-    //   xName="x"
-    //   yName="y"
-    //   dataSource={data}
-    //   // To enable tooltip for sparkline with fill color customization
-    //   tooltipSettings={{
-    //     visible: true,
-    //     format: "${x} : ${y}",
-    //     trackLineSettings: {
-    //       visible: true
-    //     },
-    //     textStyle: {
-    //       color: "white",
-    //     },
-    //   }}
-    // >
-    //   <Inject services={[SparklineTooltip]} />
-    // </SparklineComponent>
-  );
-};
+          format: "${x} : data ${y}",
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ["All"], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="y"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
+}
 
 export default SparkLine;
